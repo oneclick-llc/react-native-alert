30498e5128ac32e525d1f34fd8b7f9e54d13f894
SergeyMild <sergeymild@yandex.ru>
2023-01-05 10:48:57 +0200

    Update AlertViewManager.m

--- ios/AlertViewManager.m
+++ ios/AlertViewManager.m
@@ -26,6 +26,10 @@ RCT_EXPORT_MODULE()
     return dispatch_get_main_queue();
 }
 
++ (BOOL)requiresMainQueueSetup {
+    return true;
+}
+
 - (void)invalidate
 {
     for (UIAlertController *alertController in _alertControllers) {
