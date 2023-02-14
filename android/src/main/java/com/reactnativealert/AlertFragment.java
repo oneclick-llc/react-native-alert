e717c9327c5d0a50d3fc07ee7c273b769503be19
SergeyMild <sergeymild@yandex.ru>
2023-02-14 12:00:43 +0200

    Update AlertFragment.java

--- android/src/main/java/com/reactnativealert/AlertFragment.java
+++ android/src/main/java/com/reactnativealert/AlertFragment.java
@@ -7,15 +7,8 @@
 
 package com.reactnativealert;
 
-import android.annotation.SuppressLint;
-import android.content.DialogInterface;
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.DialogFragment;
-
 /** A fragment used to display the dialog. */
-public class AlertFragment extends DialogFragment implements DialogInterface.OnClickListener {
+public class AlertFragment {
 
   /* package */ static final String ARG_TITLE = "title";
   /* package */ static final String ARG_THEME = "theme";
@@ -27,44 +20,4 @@ public class AlertFragment extends DialogFragment implements DialogInterface.OnC
   /* package */ static final String ARG_BUTTON_NEGATIVE = "button_negative";
   /* package */ static final String ARG_BUTTON_NEUTRAL = "button_neutral";
   /* package */ static final String ARG_ITEMS = "items";
-
-  private final @Nullable AlertFragmentListener mListener;
-
-  public AlertFragment() {
-    mListener = null;
-  }
-
-  @SuppressLint("ValidFragment")
-  public AlertFragment(@Nullable AlertFragmentListener listener, Bundle arguments) {
-    mListener = listener;
-    setArguments(arguments);
-  }
-
-
-
-
-  @Override
-  public void onClick(DialogInterface dialog, int which) {
-    if (mListener != null) {
-      Bundle arguments = getArguments();
-      if (arguments == null) return;
-      int key = -1;
-      int[] buttonIndices = arguments.getIntArray("buttonIndices");
-      for (int i = 0; i < buttonIndices.length; i++) {
-        if (buttonIndices[i] == which) {
-          key = i;
-          break;
-        }
-      }
-      mListener.onClick(dialog, key);
-    }
-  }
-
-  @Override
-  public void onDismiss(DialogInterface dialog) {
-    super.onDismiss(dialog);
-    if (mListener != null) {
-      mListener.onDismiss(dialog);
-    }
-  }
 }
