254c1ccf7df2168ade5f5e82c840ee8c609278c9
SergeyMild <sergeymild@yandex.ru>
2023-02-14 12:01:15 +0200

    Update AlertViewManager.kt

--- android/src/main/java/com/reactnativealert/AlertViewManager.kt
+++ android/src/main/java/com/reactnativealert/AlertViewManager.kt
@@ -8,119 +8,105 @@ import android.os.Bundle
 import android.text.InputType
 import android.view.ContextThemeWrapper
 import android.view.View
-import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentManager
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.WhichButton
 import com.afollestad.materialdialogs.input.input
-import com.facebook.common.logging.FLog
 import com.facebook.react.bridge.*
+import java.lang.ref.WeakReference
 import java.util.*
 
-class AlertViewManager(reactContext: ReactApplicationContext?) : ReactContextBaseJavaModule(reactContext), LifecycleEventListener {
-  private var isInForeground = false
+class AlertViewManager(reactContext: ReactApplicationContext?) : ReactContextBaseJavaModule(reactContext) {
   override fun getName(): String {
     return REACT_CLASS
   }
 
-  override fun initialize() {
-    reactApplicationContext.addLifecycleEventListener(this)
+  private fun onClick(which: Int, arguments: Bundle?, callback: Callback, inputText: CharSequence? = null) {
+    if (arguments == null) return
+    var key = -1
+    val buttonIndices = arguments.getIntArray("buttonIndices")
+    for (i in buttonIndices!!.indices) {
+      if (buttonIndices[i] == which) {
+        key = i
+        break
+      }
+    }
+    if (which == Dialog.BUTTON_POSITIVE && inputText != null) callback.invoke(key, inputText.toString()) else callback.invoke(key)
+
+    presentedDialog?.clear()
+    presentedDialog = null
   }
 
-  private inner class FragmentManagerHelper(private val mFragmentManager: FragmentManager) {
-    private var mFragmentToShow: Any? = null
-    fun showPendingAlert() {
-      UiThreadUtil.assertOnUiThread()
-      SoftAssertions.assertCondition(isInForeground, "showPendingAlert() called in background")
-      if (mFragmentToShow == null) {
-        return
-      }
-      dismissExisting()
-      (mFragmentToShow as AlertFragment).show(mFragmentManager, FRAGMENT_TAG)
-      mFragmentToShow = null
-    }
 
-    private fun dismissExisting() {
-      if (!isInForeground) return
-      val oldFragment = mFragmentManager.findFragmentByTag(FRAGMENT_TAG) as AlertFragment?
-      if (oldFragment != null && oldFragment.isResumed) {
-        oldFragment.dismiss()
-      }
-    }
 
-    private fun onClick(which: Int, arguments: Bundle?, callback: Callback, inputText: CharSequence? = null) {
-      if (arguments == null) return
-      var key = -1
-      val buttonIndices = arguments.getIntArray("buttonIndices")
-      for (i in buttonIndices!!.indices) {
-        if (buttonIndices[i] == which) {
-          key = i
-          break
-        }
-      }
-      if (which == Dialog.BUTTON_POSITIVE && inputText != null) callback.invoke(key, inputText.toString()) else callback.invoke(key)
-    }
+  var presentedDialog: WeakReference<MaterialDialog>? = null
 
-    @SuppressLint("CheckResult")
-    fun showNewAlert(arguments: Bundle, actionCallback: Callback) {
-      UiThreadUtil.assertOnUiThread()
-      dismissExisting()
-      val type = arguments.getString("type")
-      val positive = arguments.getString(AlertFragment.ARG_BUTTON_POSITIVE)
-      val neutral = arguments.getString(AlertFragment.ARG_BUTTON_NEUTRAL)
-      val negative = arguments.getString(AlertFragment.ARG_BUTTON_NEGATIVE)
-      val title = arguments.getString(AlertFragment.ARG_TITLE)
-      val message = arguments.getString(AlertFragment.ARG_MESSAGE)
-      val keyBoardType = arguments.getString(AlertFragment.ARG_KEYBOARD_TYPE)
-      val theme = arguments.getString(AlertFragment.ARG_THEME)
-      val contextThemeWrapper = ContextThemeWrapper(
-        currentActivity,
-        if (theme == "light") R.style.MyDialogStyleLight else R.style.MyDialogStyleDark
-      )
-      val materialDialog = MaterialDialog(contextThemeWrapper)
-      materialDialog.cancelable(false)
-      if (title != null && title.isNotEmpty()) {
-        materialDialog.title(null, title)
-      }
-      if (message != null && message.isNotEmpty()) {
-        materialDialog.message(null, message, null)
-      } else {
-        materialDialog.view.contentLayout.visibility = View.GONE
-      }
-      if (neutral != null) {
-        materialDialog.neutralButton(text = neutral) { onClick(Dialog.BUTTON_NEUTRAL, arguments, actionCallback) }
-        materialDialog.view.buttonsLayout!!.actionButtons[WhichButton.NEUTRAL.index].typeface = Typeface.DEFAULT_BOLD
-      }
-      if (positive != null) {
-        materialDialog.positiveButton(text = positive) {
-          if (type != "default") return@positiveButton
-          onClick(Dialog.BUTTON_POSITIVE, arguments, actionCallback)
-        }
-      }
-      if (negative != null) {
-        materialDialog.negativeButton(null, negative) { onClick(Dialog.BUTTON_NEGATIVE, arguments, actionCallback) }
-        materialDialog.view.buttonsLayout!!.actionButtons[WhichButton.NEGATIVE.index].updateTextColor(Color.RED)
-      }
-      var inputType = InputType.TYPE_CLASS_TEXT
-      if (keyBoardType != null && (keyBoardType == "number-pad" || keyBoardType == "decimal-pad")) {
-        inputType = InputType.TYPE_CLASS_NUMBER
-      }
-      if (type == "secure-text") inputType = inputType or InputType.TYPE_TEXT_VARIATION_PASSWORD
-      if (type != "default") {
-        materialDialog.input(
-          prefill = arguments.getString("defaultValue"),
-          inputType = inputType,
-          waitForPositiveButton = true,
-          allowEmpty = true
-        ) { _, charSequence -> onClick(Dialog.BUTTON_POSITIVE, arguments, actionCallback, charSequence) }
-      }
-      materialDialog.show()
-    }
+  @ReactMethod
+  fun dismissTopPresented() {
+    presentedDialog?.get()?.dismiss()
+    presentedDialog?.clear()
+    presentedDialog = null
   }
+
+  @SuppressLint("CheckResult")
+  fun showNewAlert(arguments: Bundle, actionCallback: Callback) {
+    UiThreadUtil.assertOnUiThread()
+    dismissTopPresented()
+    val type = arguments.getString("type")
+    val positive = arguments.getString(AlertFragment.ARG_BUTTON_POSITIVE)
+    val neutral = arguments.getString(AlertFragment.ARG_BUTTON_NEUTRAL)
+    val negative = arguments.getString(AlertFragment.ARG_BUTTON_NEGATIVE)
+    val title = arguments.getString(AlertFragment.ARG_TITLE)
+    val message = arguments.getString(AlertFragment.ARG_MESSAGE)
+    val keyBoardType = arguments.getString(AlertFragment.ARG_KEYBOARD_TYPE)
+    val theme = arguments.getString(AlertFragment.ARG_THEME)
+    val contextThemeWrapper = ContextThemeWrapper(
+      currentActivity,
+      if (theme == "light") R.style.MyDialogStyleLight else R.style.MyDialogStyleDark
+    )
+    val materialDialog = MaterialDialog(contextThemeWrapper)
+    materialDialog.cancelable(false)
+    if (title != null && title.isNotEmpty()) {
+      materialDialog.title(null, title)
+    }
+    if (message != null && message.isNotEmpty()) {
+      materialDialog.message(null, message, null)
+    } else {
+      materialDialog.view.contentLayout.visibility = View.GONE
+    }
+    if (neutral != null) {
+      materialDialog.neutralButton(text = neutral) { onClick(Dialog.BUTTON_NEUTRAL, arguments, actionCallback) }
+      materialDialog.view.buttonsLayout!!.actionButtons[WhichButton.NEUTRAL.index].typeface = Typeface.DEFAULT_BOLD
+    }
+    if (positive != null) {
+      materialDialog.positiveButton(text = positive) {
+        if (type != "default") return@positiveButton
+        onClick(Dialog.BUTTON_POSITIVE, arguments, actionCallback)
+      }
+    }
+    if (negative != null) {
+      materialDialog.negativeButton(null, negative) { onClick(Dialog.BUTTON_NEGATIVE, arguments, actionCallback) }
+      materialDialog.view.buttonsLayout!!.actionButtons[WhichButton.NEGATIVE.index].updateTextColor(Color.RED)
+    }
+    var inputType = InputType.TYPE_CLASS_TEXT
+    if (keyBoardType != null && (keyBoardType == "number-pad" || keyBoardType == "decimal-pad")) {
+      inputType = InputType.TYPE_CLASS_NUMBER
+    }
+    if (type == "secure-text") inputType = inputType or InputType.TYPE_TEXT_VARIATION_PASSWORD
+    if (type != "default") {
+      materialDialog.input(
+        prefill = arguments.getString("defaultValue"),
+        inputType = inputType,
+        waitForPositiveButton = true,
+        allowEmpty = true
+      ) { _, charSequence -> onClick(Dialog.BUTTON_POSITIVE, arguments, actionCallback, charSequence) }
+    }
+    materialDialog.show()
+    presentedDialog = WeakReference(materialDialog)
+  }
+
 
   @ReactMethod
   fun alertWithArgs(options: ReadableMap, actionCallback: Callback) {
-    val fragmentManagerHelper = fragmentManagerHelper ?: return
     val args = Bundle()
     if (options.hasKey("theme")) {
       args.putString(AlertFragment.ARG_THEME, options.getString("theme"))
@@ -180,46 +166,7 @@ class AlertViewManager(reactContext: ReactApplicationContext?) : ReactContextBas
         }
       }
     }
-    UiThreadUtil.runOnUiThread { fragmentManagerHelper.showNewAlert(args, actionCallback) }
-  }
-
-  /**
-   * Creates a new helper to work with FragmentManager. Returns null if we're not attached to an
-   * Activity.
-   *
-   *
-   * DO NOT HOLD LONG-LIVED REFERENCES TO THE OBJECT RETURNED BY THIS METHOD, AS THIS WILL CAUSE
-   * MEMORY LEAKS.
-   */
-  private val fragmentManagerHelper: FragmentManagerHelper?
-    private get() {
-      val activity = currentActivity
-      return if (activity !is FragmentActivity) {
-        null
-      } else FragmentManagerHelper(activity.supportFragmentManager)
-    }
-
-  override fun invalidate() {
-    super.invalidate()
-    val applicationContext = reactApplicationContextIfActiveOrWarn
-    applicationContext?.removeLifecycleEventListener(this)
-  }
-
-  override fun onHostPause() {
-    // Don't show the dialog if the host is paused.
-    isInForeground = false
-  }
-
-  override fun onHostDestroy() {}
-  override fun onHostResume() {
-    isInForeground = true
-    // Check if a dialog has been created while the host was paused, so that we can show it now.
-    val fragmentManagerHelper = fragmentManagerHelper
-    if (fragmentManagerHelper != null) {
-      fragmentManagerHelper.showPendingAlert()
-    } else {
-      FLog.w(AlertViewManager::class.java, "onHostResume called but no FragmentManager found")
-    }
+    UiThreadUtil.runOnUiThread { showNewAlert(args, actionCallback) }
   }
 
   companion object {
